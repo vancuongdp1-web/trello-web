d9a13f081683619ce991e200ecd1d0885371e7e9
vancuongdp1-web <vancuongdp1@gmail.com>
2026-09-15 16:41:11 +0700

    Init Project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,28 @@
+// Updated by trungquandev.com's author on May 13 2023
+// Sample Eslint config for React project
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:react-hooks/recommended'
   ],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  plugins: [
+    'react',
+    'react-hooks',
+    'react-refresh'
+  ],
   rules: {
     'react-refresh/only-export-components': 'warn',
-  },
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'react/display-name': 0,
+
+    
+    'arrow-spacing': 1
+  }
 }
