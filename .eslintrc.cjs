ac0bf94e0f8d050e3c4275eef1877423e3812603
vancuongdp1-web <vancuongdp1@gmail.com>
2026-09-15 16:51:34 +0700

    Update Eslint Rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,7 +22,23 @@ module.exports = {
     'react/prop-types': 0,
     'react/display-name': 0,
 
-    
+    'no-console': 1,
+    'no-lonely-if': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multi-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'space-before-blocks': ['error', 'always'],
+    'object-curly-spacing': [1, 'always'],
+    'indent': ['warn', 2],
+    'semi': [1, 'never'],
+    'quotes': ['error', 'single'],
+    'array-bracket-spacing': 1,
+    'linebreak-style': 0,
+    'no-unexpected-multiline': 'warn',
+    'keyword-spacing': 1,
+    'comma-dangle': 1,
+    'comma-spacing': 1,
     'arrow-spacing': 1
   }
 }
